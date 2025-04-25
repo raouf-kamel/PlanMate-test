00aff13a9bd7058fcc4a728d24e6cb9ef17d84cb
raouf-kamel <raoufkamel2013@gmail.com>
2025-04-26 02:24:07 +0300

    add simple unit test to verify PR process

---
+++ src/test/kotlin/SampleTest.kt
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SampleTest {
+
+    @Test
+    fun `simple addition test`() {
+        val result = add(2, 3)
+        assertEquals(5, result)
+    }
+
+    private fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+}
